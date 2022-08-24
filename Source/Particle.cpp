ce1999efdaaa745871a9d88d4560f7d0519979d6
Phillip Short <phillipshort@Phillips-MBP.lan>
2022-08-24 17:54:42 +0100

    more comments

--- Source/Particle.cpp
+++ Source/Particle.cpp
@@ -17,6 +17,7 @@
 //==============================================================================
 Particle::Particle()
 {
+    //init x & y coordiantes with a random float value between 0 - 1
     x = juce::Random::getSystemRandom().nextFloat();
     y = juce::Random::getSystemRandom().nextFloat();
 }
@@ -35,15 +36,17 @@ void Particle::resized()
 
 void Particle::move(float mSliderVal, float nSliderVal, float vSliderValue)
 {
+    //chladni function found in chladni.h
     float eq = chladni(x, y, 1, 1, mSliderVal, nSliderVal);
-    
+    //multiply the value from the energy slider by the absolute value returned by chladni function
     randAmp = vSliderValue * abs(eq);
-    
+    //if energy slider is at its lowest set it to the value of minWalk so particles dont vibrate
     if (randAmp <= minWalk)
     {
         randAmp = minWalk;
     }
-    
+    //randFloatInRange function found in chladni.h
+    //add random offset to x & y coordiantes, this is what causes the particles to vibrate
     x += randFloatInRange(-randAmp, randAmp);
     y += randFloatInRange(-randAmp, randAmp);
     
@@ -52,16 +55,18 @@ void Particle::move(float mSliderVal, float nSliderVal, float vSliderValue)
 
 void Particle::updateOffsets()
 {
+   //handle edges
    if (x <= 0) x = 0;
    if (x >= 1) x = 1;
    if (y <= 0) y = 0;
    if (y >= 1) y = 1;
 
-   // convert to screen space
-   xOff = 400 * x; // (this.x + 1) / 2 * width;
-   yOff = 400 * y; // (this.y + 1) / 2 * height;
+   //convert normalised x & y coodinates to fit the size of the plugin
+   xOff = 400 * x;
+   yOff = 400 * y;
 }
 
+//getters to return the value of x & y
 float Particle::getX()
 {
     return xOff;
--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -14,12 +14,13 @@
 Chladni_VisualizerAudioProcessorEditor::Chladni_VisualizerAudioProcessorEditor (Chladni_VisualizerAudioProcessor& p)
     : AudioProcessorEditor (&p), getFunFreq(p.currentInfo), audioProcessor (p)
 {
+    //push Particle class instances into vector
     for (auto i = 0; i < numParticles; i++)
     {
         particles.push_back(std::make_unique<Particle>());
     }
     
-    
+    //set up sliders
     nSlider.setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
     nSlider.setTextBoxStyle(juce::Slider::NoTextBox, false, 0, 0);
     nSlider.setRange(1, 50, 0.01);
@@ -38,6 +39,7 @@ Chladni_VisualizerAudioProcessorEditor::Chladni_VisualizerAudioProcessorEditor (
     vSlider.addListener(this);
     addAndMakeVisible(vSlider);
     
+    //start time callback for draw loop
     Timer::startTimerHz(60);
     setSize (401, 450);
 }
@@ -50,17 +52,17 @@ Chladni_VisualizerAudioProcessorEditor::~Chladni_VisualizerAudioProcessorEditor(
 //==============================================================================
 void Chladni_VisualizerAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
+
     g.fillAll (juce::Colours::black.withLightness(0.15f));
     g.setColour(juce::Colours::black);
     g.fillRect(0, 0, 400, 400);
     g.setColour(juce::Colour::fromFloatRGBA (0.996f, 0.509f, 0.549f, 1.0f));
-    
+    //loop through particles vector and draw ellipses at each particles x & y coordinates
     for (auto i = 0; i < particles.size(); i++)
     {
         g.fillEllipse(particles[i]->getX(), particles[i]->getY(), 2, 2);
     }
-    
+    //label for sliders
     g.setColour(juce::Colours::lightslategrey);
     g.setFont(20.f);
     g.drawText("Octave", 50, 405, 100, 20, juce::Justification::centred);
@@ -75,11 +77,13 @@ void Chladni_VisualizerAudioProcessorEditor::resized()
 
 void Chladni_VisualizerAudioProcessorEditor::timerCallback()
 {
-
     repaint();
+    //getValue() returns fundamental frequency using an atomic from the audio thread
     auto frequency = getFunFreq.getValue() / x;
+    //call Particle class methods of each particle
     for (auto i = 0; i < particles.size(); i++)
     {
+        //pass frequency and both slider values to affect the mathmatical formula thus causing diffrent shapes
         particles[i]->move(frequency, nSliderVal, vSliderVal);
         particles[i]->updateOffsets();
     }
